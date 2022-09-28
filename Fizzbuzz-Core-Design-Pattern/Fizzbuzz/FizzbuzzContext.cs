48f199fae898110344a11279e780374c951c60f9
Grovre <50428844+Grovre@users.noreply.github.com>
2022-09-28 15:01:05 -0500

    Context basis

--- Fizzbuzz-Core-Design-Pattern/Fizzbuzz/FizzbuzzContext.cs
+++ Fizzbuzz-Core-Design-Pattern/Fizzbuzz/FizzbuzzContext.cs
@@ -1,7 +1,23 @@
-﻿namespace Fizzbuzz_Core_Design_Pattern.Fizzbuzz;
+﻿using Fizzbuzz_Core_Design_Pattern.Fizzbuzz.Modules;
+using Fizzbuzz_Core_Design_Pattern.Fizzbuzz.Reactions;
+using Fizzbuzz_Core_Design_Pattern.Fizzbuzz.Reactions.Determine;
+using Fizzbuzz_Core_Design_Pattern.Fizzbuzz.Reactions.Print;
+
+namespace Fizzbuzz_Core_Design_Pattern.Fizzbuzz;
 
 public class FizzbuzzContext
 {
+    private FizzbuzzNumberDeterminer _determiner;
     private FizzbuzzGame _game;
-    
+    private FizzbuzzStepPrinter _printer;
+
+    public FizzbuzzContext()
+    {
+        
+    }
+
+    public event EventHandler<FizzbuzzDetermineNumberRequest> DetermineNumberRequestHandlers;
+    public event EventHandler<FizzbuzzNumberDeterminedEvent> NumberDeterminedEventHandlers;
+    public event EventHandler<FizzbuzzPrintRequest> PrintRequestHandlers;
+    public event EventHandler<FizzbuzzPrintedEvent> PrintEventHandlers;
 }
--- Fizzbuzz-Core-Design-Pattern/Fizzbuzz/FizzbuzzGame.cs
+++ Fizzbuzz-Core-Design-Pattern/Fizzbuzz/FizzbuzzGame.cs
@@ -1,6 +1,10 @@
-﻿namespace Fizzbuzz_Core_Design_Pattern.Fizzbuzz;
+﻿using Fizzbuzz_Core_Design_Pattern.Fizzbuzz.Enums;
+
+namespace Fizzbuzz_Core_Design_Pattern.Fizzbuzz;
 
 public class FizzbuzzGame
 {
-    
+    private FizzbuzzContext _context;
+    private int _n;
+    private FizzbuzzDetermineType _determineType;
 }
